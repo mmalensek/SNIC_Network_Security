965e5855ec466fa9c354eba9038eb30e367c64fe
mmalensek <marto3304@gmail.com>
2026-02-08 20:08:25 +0100

    Testing xgboost

--- scripts/xgboost.py
+++ scripts/xgboost.py
@@ -17,12 +17,19 @@ from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer,
 def main():
 
     # loading of dataset
-    datasetLocation = "../../dataset/TrafficLabelling/Friday-DDos-SHORTENED.csv"
+    datasetLocation = "../../dataset/TrafficLabelling/Friday-DDos-SHORTENED.csv" # temp dataset
     dataframe = pd.read_csv(datasetLocation)
 
+    # loading input columns to X and target variable to y
     X = dataframe.drop(" Label", axis = 1).copy()
     y = dataframe[" Label"].copy()
-    print(y.head())
+
+    # splitting training data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
+
+    classification_xgb = xgb.XGBClassifier(objective="binary:logistic", seed=42)
+    classification_xgb.fit(X_train, y_train, verbose=True, early_stopping_rounds=10, eval_metric="aucpr", eval_set=[(X_test, y_test)])
+
 
 if __name__ == "__main__":
     main()
