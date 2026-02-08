6d0f53aab1a0e855e925fc569ed1ef7976bb8538
mmalensek <marto3304@gmail.com>
2026-02-08 18:42:10 +0100

    Testing dataset reading

--- scripts/xgboost.py
+++ scripts/xgboost.py
@@ -1,7 +1,21 @@
 """
+Simplified XGBoost implementation
+
 Prerequisites:
 pandas >= 0.25.1
 numpy >= 1.17.2
 sklearn >= 0.22.1
 xgboost >= 0.90
 """
+
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer, confusion_matrix, plot_confusion_matrix
+
+# loading of dataset
+datasetLocation = "../../dataset/TrafficLabelling/Friday-DDos-SHORTENED.csv"
+currentDataframe = pd.read_csv(datasetLocation)
+
+currentDataframe.head()
