cac38dbf7beddb307bb4d627a9d454727793a139
mmalensek <marto3304@gmail.com>
2026-02-08 18:46:23 +0100

    Fixed import

--- scripts/xgboost.py
+++ scripts/xgboost.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer, confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer, confusion_matrix
 
 # loading of dataset
 datasetLocation = "../../dataset/TrafficLabelling/Friday-DDos-SHORTENED.csv"
