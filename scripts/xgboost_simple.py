8e6be104c8f4747cea8f0798e31c9251ae7e9378
mmalensek <marto3304@gmail.com>
2026-02-11 09:52:01 +0100

    Changed dataset

--- scripts/xgboost_simple.py
+++ scripts/xgboost_simple.py
@@ -19,7 +19,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 def main():
 
     # loading of dataset
-    datasetLocation = "../../dataset/TrafficLabelling/Friday-DDos-SHORTENED.csv" # temp dataset
+    datasetLocation = "../../dataset/TrafficLabelling/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX" # temp dataset
     dataframe = pd.read_csv(datasetLocation)
 
     # converting all string to numeric
