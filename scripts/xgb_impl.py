60445cdd28c9f62bfabea0cfbdd81062528ef4e8
mmalensek <marto3304@gmail.com>
2026-02-23 21:32:53 +0100

    Added comment

--- scripts/xgb_impl.py
+++ scripts/xgb_impl.py
@@ -71,6 +71,7 @@ def main():
     print(f"Final AUCPR: {classification_xgb.best_score:.6f}")
 
     # save the model
+    print("Saving the model to classifier/xgb_model.json")
     classification_xgb.save_model("classifier/xgb_model.json")
 
 
